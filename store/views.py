3b403822fcdea7a0ce6c70b5ea129178c6d26b42
merari28 <merarivillalba@gmail.com>
2026-02-21 20:30:04 -0300

    Agregada funcionalidad de carrito, vistas y templates actualizados

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+
+from carts.models import CartItem
+from carts.views import _cart_id
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here. Updarted views.py file with the following code:
@@ -15,10 +19,13 @@ def store(request, category_slug=None):
 
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        "products": products,
+        "products": paged_products,
         "product_count": product_count,
     }
 
@@ -27,12 +34,14 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists
     except Exception as e:
         raise e 
     
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     
     return render(request, 'store/product_detail.html', context)
